ad709e9a407f0644060a069b8bfdad35286c31c6
Rui Ribeiro <ruialexrib@gmail.com>
2020-10-12 10:36:25 +0100

    -> added adminlte 3.0.5 support
    Integrating AdminLTE with ASP.NET Core 3.1 – Detailed:
    (https://www.codewithmukesh.com/blog/integrating-adminlte-with-aspnet-core/)

---
+++ Programatica.AspNetCore31AppSkeleton/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Programatica.AspNetCore31AppSkeleton.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

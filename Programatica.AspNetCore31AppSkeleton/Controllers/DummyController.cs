2001aa2480463392c9d0c7b09fd76b5bf4976318
Rui Ribeiro <ruialexrib@gmail.com>
2020-10-12 14:05:06 +0100

    -> Add Programatica.Framework support
    https://github.com/ruialexrib/Programatica.Framework

---
+++ Programatica.AspNetCore31AppSkeleton/Controllers/DummyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Programatica.AspNetCore31AppSkeleton.Data.Models;
+using Programatica.Framework.Services;
+
+namespace Programatica.AspNetCore31AppSkeleton.Controllers
+{
+    public class DummyController : Controller
+    {
+        private readonly IService<Dummy> _dummyService;
+
+        public DummyController(IService<Dummy> dummyService)
+        {
+            _dummyService = dummyService;
+        }
+
+        // GET: DummyController
+        public ActionResult Index()
+        {
+            var r = _dummyService.Get();
+            return View(r);
+        }
+
+        
+    }
+}
--- Programatica.AspNetCore31AppSkeleton/Startup.cs
+++ Programatica.AspNetCore31AppSkeleton/Startup.cs
@@ -5,9 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Programatica.AspNetCore31AppSkeleton.Data.Migrations.Context;
+using Programatica.AspNetCore31AppSkeleton.Data.Models;
+using Programatica.Framework.Core.Adapter;
+using Programatica.Framework.Data.Context;
+using Programatica.Framework.Data.Models;
+using Programatica.Framework.Data.Repository;
+using Programatica.Framework.Services;
+using Programatica.Framework.Services.Handlers;
+using Programatica.Framework.Services.Injector;
 
 namespace Programatica.AspNetCore31AppSkeleton
 {
@@ -24,6 +35,37 @@ namespace Programatica.AspNetCore31AppSkeleton
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // sqlserver context
+            // uncomment this to use sql server
+            services.AddDbContext<IDbContext, AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            // inmemory context
+            // comment this to use sql server
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(Configuration.GetConnectionString("DefaultConnection"));
+            services.AddDbContext<IDbContext, AppDbContext>(opt => opt.UseInMemoryDatabase(builder.InitialCatalog), ServiceLifetime.Transient);
+
+            // repository
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+
+            // adapters
+            services.AddTransient<IDateTimeAdapter, DateTimeAdapter>();
+            services.AddTransient<IAuthUserAdapter, AuthUserAdapter>();
+            services.AddTransient<IJsonSerializerAdapter, JsonSerializerAdapter>();
+
+            // event handler
+            services.AddTransient<IEventHandler<Dummy>, AuditEventHandler<Dummy>>();
+            services.AddTransient<IList<IEventHandler<Dummy>>>(p => p.GetServices<IEventHandler<Dummy>>().ToList());
+            services.AddTransient<IList<IEventHandler<TrackChange>>>(p => p.GetServices<IEventHandler<TrackChange>>().ToList());
+
+            // injector
+            services.AddTransient(typeof(IInjector<>), typeof(Injector<>));
+
+            // service
+            services.AddTransient(typeof(IService<>), typeof(Service<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

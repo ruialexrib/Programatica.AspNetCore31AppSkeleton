1a03c79047f4a2be52cfaa4cea7f4b8141bc1e76
Rui Ribeiro <ruialexrib@gmail.com>
2020-10-12 19:22:16 +0100

    Update Startup.cs

--- Programatica.AspNetCore31AppSkeleton/Startup.cs
+++ Programatica.AspNetCore31AppSkeleton/Startup.cs
@@ -79,7 +79,8 @@ namespace Programatica.AspNetCore31AppSkeleton
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. 
+                // You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();

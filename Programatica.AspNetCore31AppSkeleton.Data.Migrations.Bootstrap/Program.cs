71a8b5519c8298f7f67fadf78806696bffcee9aa
Rui Ribeiro <ruialexrib@gmail.com>
2020-10-13 13:41:01 +0100

    minor changes

--- Programatica.AspNetCore31AppSkeleton.Data.Migrations.Bootstrap/Program.cs
+++ Programatica.AspNetCore31AppSkeleton.Data.Migrations.Bootstrap/Program.cs
@@ -1,19 +1,5 @@
 ﻿using System;
 
-// == Package Manager Console CheatSheet ============================================================
-//
-// == Get Help ==
-// Get-Help about_EntityFrameworkCore
-// == Create Migration ==
-// Add-Migration InitialCreate -StartupProject Programatica.AspNetCore31AppSkeleton.Data.Migrations.Bootstrap
-//                             -Project Programatica.AspNetCore31AppSkeleton.Data.Migrations
-//                             -Verbose
-// == Update Database ==
-// Update-Database -StartupProject Programatica.AspNetCore31AppSkeleton.Data.Migrations.Bootstrap
-//                 -Project Programatica.AspNetCore31AppSkeleton.Data.Migrations
-//                 -Verbose
-
-
 namespace Programatica.AspNetCore31AppSkeleton.Data.Migrations.Bootstrap
 {
     class Program

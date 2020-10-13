cb549e2962f794b7064bf019f125a73b4e6cbdec
Rui Ribeiro <ruialexrib@gmail.com>
2020-10-13 22:19:05 +0100

    added bootbox plugin

--- Programatica.AspNetCore31AppSkeleton/wwwroot/js/cfg.js
+++ Programatica.AspNetCore31AppSkeleton/wwwroot/js/cfg.js
@@ -6,3 +6,11 @@ cfg.modal = cfg.modal || {};
 cfg.modal.container = "#modal-container";
 cfg.modal.dialog = "#modal-dialog";
 cfg.modal.content = "#modal-content";
+
+////////////////////////////////////
+// toast lib
+////////////////////////////////////
+
+toastr.options.closeButton = true;
+toastr.options.progressBar = false;
+toastr.options.newestOnTop = true;

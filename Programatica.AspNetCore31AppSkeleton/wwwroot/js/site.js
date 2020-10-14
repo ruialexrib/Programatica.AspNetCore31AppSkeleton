0a3dd2a3af4ce7d00363a3d2819ce1706ff5678e
Rui Ribeiro <ruialexrib@gmail.com>
2020-10-14 10:48:04 +0100

    -> js refactoring

--- Programatica.AspNetCore31AppSkeleton/wwwroot/js/site.js
+++ Programatica.AspNetCore31AppSkeleton/wwwroot/js/site.js
@@ -1,1 +1,18 @@
 ﻿
+
+$(document).on("click", ".btn", function (e) {
+    var target = $(this);
+    var text = $(target).text();
+    var atr = $(target).attr("data-loading-text");
+
+    if (atr) {
+        $(target).prop('disabled', true);
+        $(target).text(atr);
+
+        /* perform processing then reset button when done */
+        setTimeout(function () {
+            $(target).text(text);
+            $(target).prop('disabled', false);
+        }, 1000);
+    }
+});

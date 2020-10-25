a3cf4e10b753a7707369fdc741516931131b8c87
Rui Ribeiro <ruialexrib@gmail.com>
2020-10-25 18:21:39 +0000

    added basecontroller

--- Programatica.AspNetCore31AppSkeleton/Controllers/Base/BaseModelController.cs
+++ Programatica.AspNetCore31AppSkeleton/Controllers/Base/BaseModelController.cs
@@ -7,12 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Programatica.AspNetCore31AppSkeleton.ViewModels.Base;
 using Programatica.Framework.Data.Models;
+using Programatica.Framework.Mvc.Controllers;
 using Programatica.Framework.Services;
 using Syncfusion.EJ2.Base;
 
 namespace Programatica.AspNetCore31AppSkeleton.Controllers.Base
 {
-    public class BaseModelController<TModel, TViewModel, TController> : BaseController
+    public class BaseModelController<TModel, TViewModel, TController> : BaseController, EJ2DataGridBaseController<TModel>
         where TModel : IModel
         where TViewModel : class
         where TController : BaseModelController<TModel, TViewModel, TController>
@@ -35,41 +36,6 @@ namespace Programatica.AspNetCore31AppSkeleton.Controllers.Base
         public virtual IActionResult Index()
         {
             return View();
-        }
-
-        public virtual ActionResult UrlDatasource([FromBody] DataManagerRequest dm)
-        {
-            IEnumerable DataSource = _modelService.Get();
-
-            DataOperations operation = new DataOperations();
-
-            if (dm.Search != null && dm.Search.Count > 0)
-            {
-                DataSource = operation.PerformSearching(DataSource, dm.Search);  //Search
-            }
-
-            if (dm.Sorted != null && dm.Sorted.Count > 0) //Sorting
-            {
-                DataSource = operation.PerformSorting(DataSource, dm.Sorted);
-            }
-
-            if (dm.Where != null && dm.Where.Count > 0) //Filtering
-            {
-                DataSource = operation.PerformFiltering(DataSource, dm.Where, dm.Where[0].Operator);
-            }
-
-            int count = DataSource.Cast<TModel>().Count();
-
-            if (dm.Skip != 0)
-            {
-                DataSource = operation.PerformSkip(DataSource, dm.Skip);   //Paging
-            }
-            if (dm.Take != 0)
-            {
-                DataSource = operation.PerformTake(DataSource, dm.Take);
-            }
-
-            return dm.RequiresCounts ? Json(new { result = DataSource, count }) : Json(DataSource);
         }
 
         [HttpGet]

a00fbc63a34a1c7dd7efd737dd9b9d1d45e831df
Rui Ribeiro <ruialexrib@gmail.com>
2020-10-26 11:35:29 +0000

    refactor Programatica.Framework.Core.Extensions

--- Programatica.AspNetCore31AppSkeleton/Extensions/ListExtensions.cs
+++ Programatica.AspNetCore31AppSkeleton/Extensions/ListExtensions.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections.Generic;
+﻿//using System.Collections.Generic;
 
-namespace Programatica.AspNetCore31AppSkeleton.Extensions
-{
-    public static class ListExtensions
-    {
-        public static List<T> OrEmptyIfNull<T>(this List<T> source)
-        {
-            return source ?? new List<T>();
-        }
-    }
-}
+//namespace Programatica.AspNetCore31AppSkeleton.Extensions
+//{
+//    public static class ListExtensions
+//    {
+//        public static List<T> OrEmptyIfNull<T>(this List<T> source)
+//        {
+//            return source ?? new List<T>();
+//        }
+//    }
+//}

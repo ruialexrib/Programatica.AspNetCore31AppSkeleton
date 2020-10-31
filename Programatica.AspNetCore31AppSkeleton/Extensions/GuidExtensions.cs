35cf2888d8d61827a5bc1d87332e51de4fca608a
Rui Ribeiro <ruialexrib@gmail.com>
2020-10-31 02:43:24 +0000

    extension refactoring

--- Programatica.AspNetCore31AppSkeleton/Extensions/GuidExtensions.cs
+++ Programatica.AspNetCore31AppSkeleton/Extensions/GuidExtensions.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿//using System;
 
-namespace Programatica.AspNetCore31AppSkeleton.Extensions
-{
-    public static class GuidExtensions
-    {
-        public static string JsNormalize(this Guid guid)
-        {
-            var b = guid.ToByteArray();
-            b[3] |= 0xF0;
-            return new Guid(b).ToString().Replace("-", "");
-        }
-    }
-}
+//namespace Programatica.AspNetCore31AppSkeleton.Extensions
+//{
+//    public static class GuidExtensions
+//    {
+//        public static string JsNormalize(this Guid guid)
+//        {
+//            var b = guid.ToByteArray();
+//            b[3] |= 0xF0;
+//            return new Guid(b).ToString().Replace("-", "");
+//        }
+//    }
+//}
